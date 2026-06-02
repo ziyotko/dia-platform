6248bf36e45be8887d80bba030f97a3015284675
zhangpeng <zhangpeng@miic.com.cn>
2026-06-02 13:02:59 +0800

    1

--- frontend/src/views/profile/index.vue
+++ frontend/src/views/profile/index.vue
@@ -216,7 +216,7 @@ const handleChangePassword = async () => {
     pwdForm.newPassword = ''
     pwdForm.confirmPassword = ''
   } catch {
-    // request interceptor 已处理错误提示
+    ElMessage.error('密码修改失败')
   } finally {
     pwdLoading.value = false
   }
