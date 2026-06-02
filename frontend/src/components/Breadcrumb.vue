899b2bb730c9218fc6be0deb7b70af178441d1d7
zhangpeng <zhangpeng@miic.com.cn>
2026-06-02 09:08:52 +0800

    1

--- frontend/src/components/Breadcrumb.vue
+++ frontend/src/components/Breadcrumb.vue
@@ -2,13 +2,14 @@
   <el-breadcrumb separator="/">
     <el-breadcrumb-item :to="{ path: '/dashboard' }">首页</el-breadcrumb-item>
     <template v-if="route.matched.length > 1">
-      <el-breadcrumb-item
-        v-for="item in route.matched.slice(0, -1)"
-        :key="item.path"
-        :to="{ path: item.path }"
-      >
-        {{ item.meta.title }}
-      </el-breadcrumb-item>
+      <template v-for="item in route.matched.slice(0, -1)" :key="item.path">
+        <el-breadcrumb-item
+          v-if="item.meta?.title"
+          :to="{ path: item.path }"
+        >
+          {{ item.meta.title }}
+        </el-breadcrumb-item>
+      </template>
     </template>
     <el-breadcrumb-item v-if="route.meta.title && route.path !== '/dashboard'">
       {{ route.meta.title }}
