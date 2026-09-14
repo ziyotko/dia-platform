9e3e62b588d1dfb6fd12ba7769e1ada59faea369
zhangpeng <zhangpeng@miic.com.cn>
2026-09-14 09:31:19 +0800

    移除无关代码

--- business/conference/frontend/src/stores/app.ts
+++
@@ -1,8 +0,0 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
-
-export const useAppStore = defineStore('app', () => {
-  const collapsed = ref(false)
-  function toggleCollapse() { collapsed.value = !collapsed.value }
-  return { collapsed, toggleCollapse }
-})
